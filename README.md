9e5cabd1be603ac15b94dff97a127e652f85d06e
BetoIlusion <edbertoyban@hotmail.com>
2024-11-30 21:15:45 -0400

    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_movil2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_movil2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_movil2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
